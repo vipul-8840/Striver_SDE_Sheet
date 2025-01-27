76b4568a6b2facfbe93223f55e7474316a783997
vipul-8840 <vipulkumarlen@gmail.com>
2025-01-27 16:09:58 +0530

    merge intervals problem

---
+++ MergeInt.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ vector<vector<int>> merge(vector<vector<int>>& intervals)
+     {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(ans.empty()|| ans.back()[1]<intervals[i][0])
+            {
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1]  = max(intervals[i][1],ans.back()[1]);
+            }
+        }
+        return ans;
+    }
