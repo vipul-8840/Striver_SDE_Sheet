2bca8c19a0179b32d0c3d843c009655c9fb7ee66
vipul-8840 <vipulkumarlen@gmail.com>
2025-01-30 23:47:35 +0530

    two sum problem

---
+++ TwoSum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target)
+     {
+       map<int,int> mpp;
+
+    for(int i=0;i<nums.size();i++)
+    {
+        int a=nums[i];
+        int rem=target-a;
+        if(mpp.find(rem)!=mpp.end())
+        {
+            return {mpp[rem],i};
+        }
+        mpp[a]=i;
+    }
+    return {-1,-1};
+    }
