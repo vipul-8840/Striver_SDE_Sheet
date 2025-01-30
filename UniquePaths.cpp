9e623c501839f670518e88fe79652412d1aad397
vipul-8840 <vipulkumarlen@gmail.com>
2025-01-30 23:36:11 +0530

    new ways to solve this problem

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -1,21 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Dynamic Programming
+//  int countPaths(int i,int j,int m,int n, vector<vector<int>>& dp)
+//     {
+//         if(i==m-1 && j==n-1)
+//         return 1;
+//         if(i>=m || j>=n)
+//         return 0;
+//         if(dp[i][j]!=-1)
+//         return dp[i][j];
 
- int countPaths(int i,int j,int m,int n, vector<vector<int>>& dp)
-    {
-        if(i==m-1 && j==n-1)
-        return 1;
-        if(i>=m || j>=n)
-        return 0;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
+//         return dp[i][j]=countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp) ;
+//     }
+//     int uniquePaths(int m, int n) 
+//     {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
 
-        return dp[i][j]=countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp) ;
-    }
-    int uniquePaths(int m, int n) 
-    {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-
-        return countPaths(0,0,m,n,dp);
+//         return countPaths(0,0,m,n,dp);
         
+//     }
+
+// usign  combinatroics
+
+ int uniquePaths(int m, int n) 
+ {
+    int N = m+n-2;
+    int r = m-1;
+    double res = 1;
+    for(int i=1;i<=r;i++)
+    {
+        int res = res * (N-r+i)/i;
+
     }
+    return (int)res;
+ }
