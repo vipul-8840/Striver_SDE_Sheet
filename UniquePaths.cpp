615b4ff73ef376378c0ea16bb552a3641d7924a3
vipul-8840 <vipulkumarlen@gmail.com>
2025-01-30 23:08:01 +0530

    unique paths problem

---
+++ UniquePaths.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ int countPaths(int i,int j,int m,int n, vector<vector<int>>& dp)
+    {
+        if(i==m-1 && j==n-1)
+        return 1;
+        if(i>=m || j>=n)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+
+        return dp[i][j]=countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp) ;
+    }
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        return countPaths(0,0,m,n,dp);
+        
+    }
