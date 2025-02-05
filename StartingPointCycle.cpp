1031006de332f8b8b5aefab8eeb8755c49f2f27b
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-05 20:06:10 +0530

    new approach to solve this problem

--- StartingPointCycle.cpp
+++ StartingPointCycle.cpp
@@ -1,16 +1,49 @@
+// first method is hashing technique ways to check cycle detection 
+
+//  ListNode *detectCycle(ListNode *head)
+//      {
+
+//         unordered_map<ListNode * ,int> m;
+//         ListNode * curr = head;
+//         while(curr!=NULL)
+//         {
+//             if(m.find(curr)!=m.end())
+//                return curr;
+
+//                m[curr]++;
+//                curr=curr->next;
+
+//         }
+//       return NULL;
+//      }
+
+// 2nd Method (Tortoise and Hare Algorithm)
+
  ListNode *detectCycle(ListNode *head)
      {
+        ListNode* slow = head;
+        ListNode * fast = head;
+        if(head==NULL)
+         return NULL;
 
-        unordered_map<ListNode * ,int> m;
-        ListNode * curr = head;
-        while(curr!=NULL)
+        while(fast->next && fast->next->next)
         {
-            if(m.find(curr)!=m.end())
-               return curr;
+            slow = slow->next;
+            fast = fast->next->next;
+            //detect loop
+            if(slow==fast)
+            {
+                slow = head;
+                while(slow!=fast)
+                {
+                    slow=slow->next;
+                    fast = fast->next;
 
-               m[curr]++;
-               curr=curr->next;
-
+                }
+                return slow;
+            }
         }
-      return NULL;
+        return NULL;
+        
      }
+};
