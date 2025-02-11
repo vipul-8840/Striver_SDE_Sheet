e53e66ba41e9a0eed3699389e8238df38b86374f
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-11 22:39:08 +0530

    new approach to solves problem trapping raninwater problem

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -36,4 +36,38 @@ using namespace std;
 //     return total;
 // }
 
-// m-3 method without using space complexity
+// m-2 method without using space complexity
+int trap(vector<int>& height)
+     {
+        int n = height.size();
+        int left_max = 0,total = 0 ;
+        int right_max =0 ;
+        int l = 0, r = n-1;
+
+        while(l<r)
+        {
+            if(height[l]<=height[r])
+            {
+                if(left_max>height[l])
+                {
+                    total+=left_max - height[l];
+                }
+                else {
+                    left_max = height[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(right_max>height[r])
+                {
+                    total+=right_max - height[r];
+                }
+                else {
+                    right_max = height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
