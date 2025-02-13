d6db9a7d6e8323c5dcf3663b0665390ce7dc2cd6
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-13 23:20:06 +0530

    random method

--- CloneLL.cpp
+++ CloneLL.cpp
@@ -1,24 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+// m-1 brute force approach to solve to the clone a liked list problem solved 
 
-Node* copyRandomList(Node* head) 
-{
-    unordered_map<Node*,Node*> m;
-    Node * curr = head;
-    while(curr)
-    {
-        Node * temp = new Node (curr->val);
-        m[curr] = temp;
-    curr = curr->next;
 
-    }
-    curr = head;
-    while(curr)
-    {
-        Node * temp = m[curr];
-        temp->next = m[curr->next];
-        temp->random = m[curr->random];
-        curr = curr->next;
-    }
-    return m[head];
-}
+// Node* copyRandomList(Node* head) 
+// {
+//     unordered_map<Node*,Node*> m;
+//     Node * curr = head;
+//     while(curr)
+//     {
+//         Node * temp = new Node (curr->val);
+//         m[curr] = temp;
+//     curr = curr->next;
+
+//     }
+//     curr = head;
+//     while(curr)
+//     {
+//         Node * temp = m[curr];
+//         temp->next = m[curr->next];
+//         temp->random = m[curr->random];
+//         curr = curr->next;
+//     }
+//     return m[head];
+// }
+
+// m-2 method solved to problem 
+
+
