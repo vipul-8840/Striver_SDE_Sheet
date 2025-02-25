64200ed0bc89420178e64682bc8553038b850fda
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-25 23:38:41 +0530

    Minimum Bit Flips to Convert Number  problem solved

--- BitManipulation/MIniumBit.cpp
+++ BitManipulation/MIniumBit.cpp
@@ -1,1 +1,16 @@
-#
+#include<bits/stdc++.h>
+using namespace std ;
+int minBitFlips(int start, int goal)
+{
+   
+   int ans = start ^ goal;
+   int count=0;
+   while(ans)
+   {
+    if(ans%2==1)
+    count++;
+    ans/=2;
+   }
+   return count ;
+    
+}
