43e81b4eae8860d862c2af48943a5cac54ac5445
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-27 22:21:17 +0530

    new approach to solved the problem

--- BitManipulation/Primefactor.cpp
+++ BitManipulation/Primefactor.cpp
@@ -1,38 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int>AllPrimeFactors(int N)
-	{
-	    // Code here
-	    if(N==1)
-	    {
-	        return {};
-	    }
-	    vector<int>ans;
-	     vector<int>res;
-	    for(int i=2;i<=N/2;i++)
-	    {
-	        if(N%i==0)
-	        ans.push_back(i);
+// vector<int>AllPrimeFactors(int N)
+// 	{
+// 	    // Code here
+// 	    if(N==1)
+// 	    {
+// 	        return {};
+// 	    }
+// 	    vector<int>ans;
+// 	     vector<int>res;
+// 	    for(int i=2;i<=N/2;i++)
+// 	    {
+// 	        if(N%i==0)
+// 	        ans.push_back(i);
 	        
-	    }
-	    ans.push_back(N);
+// 	    }
+// 	    ans.push_back(N);
 	    
-	   for(int i= 0;i<ans.size();i++)
-	   {
-	       int digit = ans[i];
-	       int flag = 1;
-	       for(int i=2;i*i<=digit;i++)
-	       {
-	           if(digit%i==0)
-	           {
-	               flag = 0;
-	               break;
-	           }
-	       }
-	       if(flag==1)
-	       res.push_back(digit);
+// 	   for(int i= 0;i<ans.size();i++)
+// 	   {
+// 	       int digit = ans[i];
+// 	       int flag = 1;
+// 	       for(int i=2;i*i<=digit;i++)
+// 	       {
+// 	           if(digit%i==0)
+// 	           {
+// 	               flag = 0;
+// 	               break;
+// 	           }
+// 	       }
+// 	       if(flag==1)
+// 	       res.push_back(digit);
 	       
-	   }
-	   return res;
+// 	   }
+// 	   return res;
+// 	}
+
+//minimu time complexity (sqrtN)*(logN)
+
+vector<int>AllPrimeFactors(int N) {
+	// Code here
+	vector<int>ans;
+	for(int i=2;i*i<=N;i++)
+	{
+		if(N%i==0)
+		{
+			ans.push_back(i);
+			while(N%i==0)
+			{
+				N = N/i;
+			}
+		}
 	}
+	if(N!=1)
+	ans.push_back(N);
+	
+	return ans;
+}
