ce3410ebccdad82f7dc8cf1cfd28cc212a8c93a8
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-27 22:22:10 +0530

    new

--- BitManipulation/Primefactor.cpp
+++ BitManipulation/Primefactor.cpp
@@ -39,22 +39,22 @@ using namespace std;
 
 //minimu time complexity (sqrtN)*(logN)
 
-vector<int>AllPrimeFactors(int N) {
-	// Code here
-	vector<int>ans;
-	for(int i=2;i*i<=N;i++)
-	{
-		if(N%i==0)
-		{
-			ans.push_back(i);
-			while(N%i==0)
-			{
-				N = N/i;
-			}
-		}
-	}
-	if(N!=1)
-	ans.push_back(N);
+// vector<int>AllPrimeFactors(int N) {
+// 	// Code here
+// 	vector<int>ans;
+// 	for(int i=2;i*i<=N;i++)
+// 	{
+// 		if(N%i==0)
+// 		{
+// 			ans.push_back(i);
+// 			while(N%i==0)
+// 			{
+// 				N = N/i;
+// 			}
+// 		}
+// 	}
+// 	if(N!=1)
+// 	ans.push_back(N);
 	
-	return ans;
-}
+// 	return ans;
+// }
