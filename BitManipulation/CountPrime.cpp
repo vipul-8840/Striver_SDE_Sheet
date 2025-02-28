d2edd5fb01e16de0eaf240dd1ed5b94406cdc175
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-28 22:46:05 +0530

    count prime problem solved

---
+++ BitManipulation/CountPrime.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int countPrimes(int n) 
+    {
+        int count = 0;
+       vector<int>arr(n,1);
+        for(int i=2;i*i<=n;i++)
+        {
+            if(arr[i]==1)
+            {
+             
+                for(int j = i*i ;j<n;j=j+i)
+                {
+                    arr[j]=0;
+                }
+            }
+        }
+        for(int i=2;i<n;i++)
+        {
+            if(arr[i]==1)
+            count++;
+        }
+        return count ;
+        
+    }
+
+    
