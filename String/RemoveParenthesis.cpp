79bdf8a5fecce888e4fd2ac075821f165ece27a6
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-01 13:56:40 +0530

    2nd approached to solved this problem

--- String/RemoveParenthesis.cpp
+++ String/RemoveParenthesis.cpp
@@ -1,26 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std ;
-string removeOuterParentheses(string s)
+// string removeOuterParentheses(string s)
+// {
+//    stack<char>st;
+//    string ans = "";
+//    for(int i=0;i<s.size();i++)
+//    {
+//       if(s[i]=='(')
+//       {
+//        if(!st.empty())
+//        {
+//            ans+='(';
+//        }
+//        st.push('(');
+//       }
+//       else{
+//            st.pop();
+//            if(!st.empty())
+//            {
+//                ans+=')';
+//            }
+//       }
+//    }
+//    return ans ;
+// }
+
+//2nd method to solved this problem without using extra space time complexity 
+
+string removeOuterParentheses(string s) 
 {
-   stack<char>st;
-   string ans = "";
-   for(int i=0;i<s.size();i++)
-   {
-      if(s[i]=='(')
-      {
-       if(!st.empty())
-       {
-           ans+='(';
-       }
-       st.push('(');
-      }
-      else{
-           st.pop();
-           if(!st.empty())
-           {
-               ans+=')';
-           }
-      }
-   }
-   return ans ;
+    int count = 0;
+    string ans="";
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]=='(')
+        {
+            if(count==0)
+             count++;
+             else{
+                ans+="(";
+                count++;
+             }
+        }
+        else{
+            count--;
+            if(count>0)
+            {
+                ans+=')';
+            }
+        }
+    }
+    return ans ;
 }
