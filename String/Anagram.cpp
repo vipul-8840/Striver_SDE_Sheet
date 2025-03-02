011fbbcf9a6737dd67b5c8e78c6ed9d3c166aba3
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-02 22:40:25 +0530

    Valid Anagram

---
+++ String/Anagram.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std ;
+bool isAnagram(string s, string t) {
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    if(s==t)
+    return 1;
+
+    return 0;
+
+}
