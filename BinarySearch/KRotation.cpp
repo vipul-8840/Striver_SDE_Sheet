78006f6e9b608d9ce4157cd91c59e638cd29fd80
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-12 03:19:34 +0530

    Search in Rotated Sorted Array

--- BinarySearch/KRotation.cpp
+++ BinarySearch/KRotation.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 // m-1 solve the problem in liner time complexity
 int findKRotation(vector<int> &arr) {
-   int n = arr.size();
-  for(int i=n-1;i>0;i--)
-  {
-      if(arr[i]<arr[i-1])
-        return i-0;
-  }
+//    int n = arr.size();
+//   for(int i=n-1;i>0;i--)
+//   {
+//       if(arr[i]<arr[i-1])
+//         return i-0;
+//   }
     
-    return 0;
+//     return 0;
     
 }
---
+++ BinarySearch/SearchSorted.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int n = nums.size();
+    int start = 0;
+    int end = n-1;
+    while(start<=end)
+    {
+        int mid = start + (end-start)/2;
+
+        if(nums[mid]==target)
+        return mid;
+
+        else if (nums[start]<=nums[mid])
+        {
+            if(nums[start]<=target && target<=nums[mid])
+            {
+                end = mid-1;
+            }
+            else {
+                start = mid + 1; 
+            }
+        }
+        else
+        {
+             if(nums[mid]<=target && target<=nums[end])
+            {
+                start=mid+1;
+            }
+            else {
+              end= mid -1 ; 
+            }
+
+        }
+
+    }
+    return -1;
+}
