4d45ba6c7e50a19841375f7a8f8eac1372620569
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-12 00:14:36 +0530

    Find nth root of m

--- BinarySearch/NRoot.cpp
+++ BinarySearch/NRoot.cpp
@@ -2,19 +2,34 @@
 using namespace std ;
 
 int nthRoot(int n, int m) {
-    // Code here.
-    int ans = -1;
+    // Code here. for linear search approach
+//     int ans = -1;
     
-    for(int i = 1;i<=m;i++)
-    {
-          int power = pow(i,n);
-          if(power==m)
-        {
-             ans = i ;
-              break;
-        }
-          if(power>m)
-          break;
+//     for(int i = 1;i<=m;i++)
+//     {
+//           int power = pow(i,n);
+//           if(power==m)
+//         {
+//              ans = i ;
+//               break;
+//         }
+//           if(power>m)
+//           break;
+//     }
+//     return ans;
+// method -2 with the help of binary search
+int start = 1;
+int end = m;
+while(start <=end)
+{
+    int mid = start + (end-start)/2;
+    if(pow(mid,n)==m)
+    return mid ;
+    else if (pow(mid,n)<m)
+    start = mid+1;
+    else {
+        end = mid-1;
     }
-    return ans;
+}
+  return -1;
 }
--- BinarySearch/SquareRoot.cpp
+++ BinarySearch/SquareRoot.cpp
@@ -1,23 +1,20 @@
 #include<bits/stdc++.h>
-using namespace std;
-
+using namespace std ;
 int floorSqrt(int n) {
-    // Initialize start and end for binary search
+    // Your code goes here
     int start = 1;
     int end = n;
     int ans = 1;
-
-    // Perform binary search
-    while(start <= end) {
-        int mid = start + (end - start) / 2;
-
-        // Check if mid*mid is less than or equal to n
-        if(mid * mid <= n) {
-            ans = mid; // Update answer
-            start = mid + 1; // Move to the right half
-        } else {
-            end = mid - 1; // Move to the left half
+    while(start<=end)
+    {
+        int mid = start + (end-start)/2;
+        if(mid*mid<=n)
+        {
+            ans = mid ;
+            start = mid+1;
         }
+        else  
+        end = mid-1;
     }
-    return ans; // Return the floor value of the square root
+    return ans;
 }
