302799e679cad761cf500d5226797fbdc4bbfa6d
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-13 02:41:36 +0530

    Minimum in Rotated Sorted Array II

--- BinarySearch/MinRotated.cpp
+++ BinarySearch/MinRotated.cpp
@@ -10,3 +10,32 @@ int findMin(vector<int>& nums) {
     }
     return ans;
 }
+
+//Binary Seacrh approach to solve this problem 
+int findMin(vector<int>& nums) {
+int start = 0;
+int end = nums.size()-1;
+int ans = INT_MAX;
+while(start<=end)
+{
+    int mid = start + (end-start)/2;
+    if(nums[start]<=nums[end])
+    {
+        ans = min(ans,nums[start]);
+        break;
+    }
+
+    else if (nums[start]<=nums[mid])
+    {
+        ans = min(ans,nums[start]);
+        start = mid +1;
+    }
+    else {
+
+        ans = min(ans,nums[mid]);
+        end = mid-1;
+
+    }
+}
+return ans;
+}
