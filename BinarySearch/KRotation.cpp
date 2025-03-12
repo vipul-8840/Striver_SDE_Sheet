4da857bfe7a0050148249ef5fdb6eb049c41e274
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-13 02:54:57 +0530

    Binary Search approach

--- BinarySearch/KRotation.cpp
+++ BinarySearch/KRotation.cpp
@@ -13,3 +13,49 @@ int findKRotation(vector<int> &arr) {
 //     return 0;
     
 }
+// m-2 Binary Search method 
+int findKRotation(vector<int> &nums) {
+   
+    
+     int start = 0;
+    int end = nums.size()-1;
+    int index = 0;
+    int ans = INT_MAX;
+    while(start<=end)
+    {
+        int mid = start + (end-start)/2;
+        // if(nums[start]<=nums[end])
+        // {
+        //     if(ans<=nums[start])
+        //     {
+        //         ans = nums[start];
+        //         index = start;
+        //     }
+        //     break;
+        // }
+
+         if (nums[start]<=nums[mid])
+        {
+            // ans = min(ans,nums[start]);
+            if(ans>=nums[start])
+            {
+                ans = nums[start];
+                index = start;
+            }
+            start = mid +1;
+        }
+        else {
+
+            // ans = min(ans,nums[mid]);
+            if(ans>=nums[mid])
+            {
+                ans = nums[mid];
+                index = mid;
+            }
+            end = mid-1;
+
+        }
+    }
+    return index-0;
+
+}
