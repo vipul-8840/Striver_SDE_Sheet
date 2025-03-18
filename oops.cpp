d13062138a799f250f7cd9b9a1f203bc2cce0c49
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-18 23:41:19 +0530

    oops concepts

---
+++ oops.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std ;
+// class Add 
+// {
+//  public:
+//   int add(int a,int b)
+//   {
+//      return a + b;
+//   }
+//   int add(int a,int b ,int c)
+//   {
+//     return (a+b+c);
+//   }
+// };
+// int main ()
+// {
+//     Add obj;
+//     int res1 = obj.add (2,3);
+//     int res2 = obj.add(2,3,4);
+//     cout<<res1<<" "<<res2<<endl;
+//     return 0;
+
+// }
+  
+// class base {
+//     public :
+//     virtual void show ()
+//     {
+//         cout<<"this is parent class"<<endl;
+//     }
+// };
+
+// class child : public base
+// { 
+  
+//   public:
+//   string name ;
+//     void show()
+//     {
+//         cout<<"this is child class "<<endl;
+//     }
+
+// };
+// int main ()
+// {
+//     base * ptr ;
+//     ptr->show();
+//     child a ;
+//     a.name = "vipul";
+//     ptr = &a;
+//     a.show();
+//     ptr->show();
+//     return 0;
+
+// }
+
+// class add {
+//     public:
+//     int a,b;
+//     add ()
+//     {
+         
+//     }
+//     add(int a ,int b )
+//     {
+//         this->a = a;
+//         this->b = b;
+//     }
+    
+// };
+// int main ()
+// {
+//     add obj(5,6);
+//     cout<<obj.a <<" "<<obj.b <<endl;
+// }
+
+
+// class A{
+//     int a = 2;
+//     int b = 4;
+//     public :
+//     friend int add (A k)
+//     {
+//         return k.a + k.b;
+//     }
+// };
+// int main ()
+// {
+//     A obj;
+//     int res = add(obj);
+//     cout<<res <<endl;
+// }
+
+
