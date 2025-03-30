52dd21403f7b7fe36e758f5f1b370ce7ed326f8d
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-31 02:37:36 +0530

    sorted Stack

---
+++ Recursion/sortedStack.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ivoid sorted(stack<int>&s,int num)
+{
+    if(s.empty() || num>s.top())
+    {
+        s.push(num);
+        return ;
+    }
+    int a = s.top();
+    s.pop();
+    sorted(s,num);
+    s.push(a);
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   if(s.empty())
+   return ;
+   int num = s.top();
+   s.pop();
+   sort();
+   sorted(s,num);
+   
+}
