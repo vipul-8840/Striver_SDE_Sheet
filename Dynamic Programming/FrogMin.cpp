dbb901584059532f5e5a7e83d707fa104dd7fdfb
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-11 10:43:52 +0530

    recursion problem

---
+++ Dynamic Programming/FrogMin.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int find(vector<int>&arr,int index,vector<int>&dp)
+{
+   if(index==0)
+   return 0;
+   if(dp[index]!=-1)
+    return dp[index];
+   int l = find(arr,index-1,dp) + abs(arr[index]-arr[index-1]);
+   int r = INT_MAX;
+   if(index>1)
+   r = find(arr,index-2,dp) + abs(arr[index]-arr[index-2]);
+
+   return dp[index]= min(l,r);
+}
+
+int main ()
+{
+    int n ;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter the element in array : ";
+    vector<int>dp(n,-1);
+    for(int i=0;i<n;i++)
+    { 
+        cin>>arr[i];
+    }
+     cout<<"minium energy required to reach last index : "<<find(arr,n-1,dp);
+     return 0;
+
+}
