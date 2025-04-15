ab9201017023136062d18bd5a398c75bafcf7e06
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-15 12:25:41 +0530

    correct sol

--- Recursion/SubsetSum.cpp
+++ Recursion/SubsetSum.cpp
@@ -1,9 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std ;
+//  int main ()
+//  {
+//     int n ;
+//     cout<<" Enter the size of the array is : "<<endl;
+//     cin>>n;
+//     vector<int>arr(n);
+//     for(int i=0;i<n;i++)
+//     {
+//         cin>>arr[i];
+//     }
+
+//     for(int i=0;i<n;i++)
+//     {
+//         int sum = 0;
+//         for(int j=i;j<n;j++)
+//         {
+//             sum+=arr[j];
+//             cout<<sum<<" "; 
+//         }
+//     }
+//     return 0;
+//  }
+ void find(vector<int>&arr,int index,int sum ,int n)
+ {
+    if(index==n)
+    {
+        cout<<sum<<" ";
+        return ;
+    }
+
+    find(arr,index+1,sum+arr[index],n);
+    find(arr,index+1,sum,n);
+ }
  int main ()
  {
     int n ;
-    cout<<" Enter the size of the array is : "<<endl;
+    cout<<" Enter the size of the array is : ";
     cin>>n;
     vector<int>arr(n);
     for(int i=0;i<n;i++)
@@ -11,14 +44,5 @@ using namespace std ;
         cin>>arr[i];
     }
 
-    for(int i=0;i<n;i++)
-    {
-        int sum = 0;
-        for(int j=i;j<n;j++)
-        {
-            sum+=arr[j];
-            cout<<sum<<" "; 
-        }
-    }
-    return 0;
- }
+     find (arr,0,0,n);
+}
