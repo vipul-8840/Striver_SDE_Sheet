b595ff16cfe84d0621c2309b6b596101267dce45
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-19 20:02:30 +0530

    iterative

--- Tree/Inorder.cpp
+++ Tree/Inorder.cpp
@@ -1,17 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void inorder(TreeNode* root,vector<int>&ans)
-     {
-        if(root==NULL)
-        return ;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-     }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        inorder(root,ans);
-        return ans;
+// void inorder(TreeNode* root,vector<int>&ans)
+//      {
+//         if(root==NULL)
+//         return ;
+//         inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         inorder(root->right,ans);
+//      }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         inorder(root,ans);
+//         return ans;
         
+//     }
+
+//iterative order traversal to solve this problem 
+vector<int> inorderTraversal(TreeNode* root) {
+    TreeNode* curr = root;
+    vector<int>ans;
+    if(root==NULL)
+    return ans;
+    stack<TreeNode*>st;
+    while(true)
+    {
+       if(curr)
+       {
+           st.push(curr);
+           curr =curr->left ; 
+       }
+       else{
+           if(st.empty())
+           {
+               break;
+           }
+           curr = st.top();
+            st.pop();
+           ans.push_back(curr->val);
+           curr = curr->right ;
+       }
+    
     }
+    return ans;
+    
+}
