99cf095de70f99346da86a5905f7209d48810a27
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-19 18:12:26 +0530

    with help of stack

--- Tree/Preorder.cpp
+++ Tree/Preorder.cpp
@@ -1,19 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std ;
-void preorder(TreeNode* root,vector<int>&ans)
-{
-    if(root==NULL)
-    return ;
+// #include<bits/stdc++.h>
+// using namespace std ;
+// void preorder(TreeNode* root,vector<int>&ans)
+// {
+//     if(root==NULL)
+//     return ;
 
-    ans.push_back(root->val);
-    preorder(root->left,ans);
-    preorder(root->right,ans);
+//     ans.push_back(root->val);
+//     preorder(root->left,ans);
+//     preorder(root->right,ans);
 
-}
-vector<int> preorderTraversal(TreeNode* root)
- {
-       vector<int> ans ;
-       preorder(root,ans);
-       return ans ;
+// }
+// vector<int> preorderTraversal(TreeNode* root)
+//  {
+//        vector<int> ans ;
+//        preorder(root,ans);
+//        return ans ;
     
+// }
+
+//iterative preorder traversal 
+vector<int> preorderTraversal(TreeNode* root) {
+    stack<TreeNode *> s;
+
+     vector<int> ans;
+     if(root==NULL)
+     return ans;
+    s.push(root);
+    while(!s.empty())
+    {
+        TreeNode* curr = s.top();
+        s.pop();
+        ans.push_back(curr->val);
+        if(curr->right)
+        s.push(curr->right);
+        if(curr->left)
+        s.push(curr->left);
+    }
+    return ans;
 }
