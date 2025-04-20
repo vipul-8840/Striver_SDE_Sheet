95a8c7041ec00a90c2ef234280614e096f72d2a9
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-20 12:16:11 +0530

    using two stack

--- Tree/Postorder.cpp
+++ Tree/Postorder.cpp
@@ -19,25 +19,51 @@ using namespace std;
      
 //  }
 // using stack 
+// vector<int> postorderTraversal(TreeNode* root) {
+//     vector<int>ans;
+//     if(root==NULL)
+//     return ans ;
+//     stack<TreeNode *> s;
+//     s.push(root);
+
+//     while(!s.empty())
+//     { 
+//         TreeNode * curr = s.top();
+//         s.pop();
+//         ans.push_back(curr->val);
+//         if(curr->left)
+//         s.push(curr->left);
+//         if(curr->right)
+//         s.push(curr->right);
+
+//     }
+//     reverse(ans.begin(),ans.end());
+//     return  ans;
+    
+// }
+
+//using two stack
 vector<int> postorderTraversal(TreeNode* root) {
     vector<int>ans;
     if(root==NULL)
-    return ans ;
-    stack<TreeNode *> s;
-    s.push(root);
-
-    while(!s.empty())
-    { 
-        TreeNode * curr = s.top();
-        s.pop();
-        ans.push_back(curr->val);
-        if(curr->left)
-        s.push(curr->left);
-        if(curr->right)
-        s.push(curr->right);
-
+    return ans;
+    stack<TreeNode *> s1;
+    stack<TreeNode *>s2;
+    s1.push(root);
+    while(!s1.empty())
+    {
+         TreeNode* curr = s1.top();
+         s1.pop();
+         s2.push(curr);
+         if(curr->left)
+         s1.push(curr->left);
+          if(curr->right)
+         s1.push(curr->right);
     }
-    reverse(ans.begin(),ans.end());
-    return  ans;
-    
+    while(!s2.empty())
+    {
+        ans.push_back(s2.top()->val);
+        s2.pop();
+    }
+    return ans;
 }
