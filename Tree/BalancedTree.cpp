26d5749049d84e2353c5f7873d8effe4dae95688
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-20 20:12:16 +0530

    individaul height cal

--- Tree/BalancedTree.cpp
+++ Tree/BalancedTree.cpp
@@ -1,30 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int findHeight(TreeNode* root)
+//     {
+//         if(root==NULL)
+//         return 0;
 
+//         return 1 + max(findHeight(root->left),findHeight         (root->right));
+//     }
+//  bool Balanced(TreeNode * root)
+//  {
+//     if(root==NULL)
+//     return true;
+//     int l = findHeight(root->left);
+//     int r = findHeight(root->right);
+//     if(abs(l-r)>1)
+//     return false ;
+//     return Balanced(root->left) && Balanced(root->right);
+//  }
+// class Solution
+//  {
+// public:
+//        bool isBalanced(TreeNode* root) {
+//         return Balanced(root);
+//     }
+// };
 
-
-int findHeight(TreeNode* root)
+// O(n)
+int findHeight(TreeNode * root)
     {
         if(root==NULL)
         return 0;
 
-        return 1 + max(findHeight(root->left),findHeight         (root->right));
+        int l = findHeight(root->left);
+        if(l==-1)
+        return -1;
+        int r = findHeight(root->right);
+        if(r==-1)
+        return -1;
+
+        if(abs(l-r)>1) return -1;
+
+        return 1 + max(l,r);
+
     }
- bool Balanced(TreeNode * root)
- {
-    if(root==NULL)
-    return true;
-    int l = findHeight(root->left);
-    int r = findHeight(root->right);
-    if(abs(l-r)>1)
-    return false ;
-    return Balanced(root->left) && Balanced(root->right);
- }
-class Solution
- {
-public:
-       bool isBalanced(TreeNode* root) {
-        return Balanced(root);
+    bool isBalanced(TreeNode* root) {
+        return findHeight(root) != -1;
     }
-};
