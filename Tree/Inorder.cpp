bf7b277b9c382652a3fbbc6d2a5fd5a5d36069c3
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-25 10:50:32 +0530

    moris traversal

--- Tree/Inorder.cpp
+++ Tree/Inorder.cpp
@@ -1,47 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// void inorder(TreeNode* root,vector<int>&ans)
-//      {
-//         if(root==NULL)
-//         return ;
-//         inorder(root->left,ans);
-//         ans.push_back(root->val);
-//         inorder(root->right,ans);
-//      }
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         vector<int>ans;
-//         inorder(root,ans);
-//         return ans;
+// // void inorder(TreeNode* root,vector<int>&ans)
+// //      {
+// //         if(root==NULL)
+// //         return ;
+// //         inorder(root->left,ans);
+// //         ans.push_back(root->val);
+// //         inorder(root->right,ans);
+// //      }
+// //     vector<int> inorderTraversal(TreeNode* root) {
+// //         vector<int>ans;
+// //         inorder(root,ans);
+// //         return ans;
         
-//     }
+// //     }
 
-//iterative order traversal to solve this problem 
-vector<int> inorderTraversal(TreeNode* root) {
-    TreeNode* curr = root;
-    vector<int>ans;
-    if(root==NULL)
-    return ans;
-    stack<TreeNode*>st;
-    while(true)
-    {
-       if(curr)
+// //iterative order traversal to solve this problem 
+// vector<int> inorderTraversal(TreeNode* root) {
+//     TreeNode* curr = root;
+//     vector<int>ans;
+//     if(root==NULL)
+//     return ans;
+//     stack<TreeNode*>st;
+//     while(true)
+//     {
+//        if(curr)
+//        {
+//            st.push(curr);
+//            curr =curr->left ; 
+//        }
+//        else{
+//            if(st.empty())
+//            {
+//                break;
+//            }
+//            curr = st.top();
+//             st.pop();
+//            ans.push_back(curr->val);
+//            curr = curr->right ;
+//        }
+    
+//     }
+//     return ans;
+    
+// }
+
+
+//Morris order Traversal 
+vector<int> inorderTraversal(TreeNode* root)
+{
+   vector<int>ans;
+   if(root==NULL)
+   return ans ;
+   while(root)
+   {
+       if(!root->left)
        {
-           st.push(curr);
-           curr =curr->left ; 
+           ans.push_back(root->val);
+           root=root->right;
        }
        else{
-           if(st.empty())
-           {
-               break;
-           }
-           curr = st.top();
-            st.pop();
-           ans.push_back(curr->val);
-           curr = curr->right ;
+             TreeNode * curr = root->left;
+             while(curr->right && curr->right!=root)
+             {
+                  curr = curr->right;
+             }
+             if(!curr->right)
+             {
+               curr->right = root;
+               root=root->left;
+             }
+             else{
+               curr->right=NULL;
+               ans.push_back(root->val);
+               root=root->right;
+             }
        }
-    
-    }
-    return ans;
-    
+   }
+   return ans;
 }
