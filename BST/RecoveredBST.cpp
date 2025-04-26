fe041071934999aefd083f48e30a6e8cc24f4590
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-26 19:27:56 +0530

    Recover BST

---
+++ BST/RecoveredBST.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class TreeNode{
+    public:
+    int val ;
+    TreeNode*left;
+    TreeNode* right;
+    TreeNode(int val)
+    {
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void inorder(TreeNode * root,vector<int>&ans)
+{
+     if(root==NULL)
+     return ;
+     inorder(root->left,ans);
+     ans.push_back(root->val);
+     inorder(root->right,ans);
+
+
+}
+void pushed(TreeNode* root,stack<TreeNode *> & st)
+{
+    if(root==NULL)
+    return ;
+  
+    while(root)
+    {
+        st.push(root);
+        root= root->left;
+    }
+ return ;
+
+}
+void recoverTree(TreeNode* root)
+ {
+      vector<int>ans;
+      inorder(root,ans);
+      sort(ans.begin(),ans.end());
+      stack<TreeNode*>st;
+      pushed(root,st);
+      int n = ans.size();
+      for(int i=0;i<n;i++)
+      {
+          if(ans[i]==st.top()->val)
+          {
+                TreeNode * node = st.top();
+                 st.pop();
+                pushed(node->right,st);
+          }
+          else{
+                  TreeNode* node = st.top();
+                  node->val = ans[i];
+                  st.pop();
+                  pushed(node->right,st);
+          }
+
+      }
+   return ;
+    
+}
