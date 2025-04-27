b05a863fdf5620cdf387574b84e5618b0af001ff
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-27 23:48:28 +0530

    totoise

---
+++ LinkedList/Tortoise.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ListNode* middleNode(ListNode* head)
+     {
+        if(head==NULL || head->next==NULL)
+        return head;
+        ListNode * slow = head ;
+        ListNode * fast = head ;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow = slow->next ;
+            fast = fast->next->next ;
+        }
+        return slow;
+    }
