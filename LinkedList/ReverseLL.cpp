e3530b8f04fb843d93e824d31f75c9b68b40c881
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-27 22:05:36 +0530

    recursive way

--- LinkedList/ReverseLL.cpp
+++ LinkedList/ReverseLL.cpp
@@ -1,20 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ListNode* reverseList(ListNode* head) {
+// ListNode* reverseList(ListNode* head) {
 
-    if(head==NULL || head->next ==NULL)
-    return head;
+//     if(head==NULL || head->next ==NULL)
+//     return head;
 
-    ListNode * prev = NULL;
-    ListNode * curr = head;
-    while(curr)
-    {
-        ListNode * temp = curr->next;
-        curr->next = prev ;
-        prev = curr ;
-        curr = temp;
-    }
-    return prev;
+//     ListNode * prev = NULL;
+//     ListNode * curr = head;
+//     while(curr)
+//     {
+//         ListNode * temp = curr->next;
+//         curr->next = prev ;
+//         prev = curr ;
+//         curr = temp;
+//     }
+//     return prev;
+
+// }
+ListNode* reverseList(ListNode* head)
+{
+   if(head==NULL || head->next==NULL)
+   return head ;
+
+   ListNode * newHead = reverseList(head->next);
+   ListNode * temp = head->next;
+   temp->next = head;
+   head->next = NULL;
+   return newHead;
+
 
 }
