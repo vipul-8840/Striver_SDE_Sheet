65a595eb92e2956ab9db505fc6b5fb289b4f2a72
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-28 13:29:55 +0530

    cycle

---
+++ LinkedList/LinkedCycle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool hasCycle(ListNode *head)
+     {
+       
+        if(head==NULL || head->next==NULL)
+        return false;
+        ListNode* slow = head ;
+        ListNode * fast = head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+      
+            slow = slow->next;
+            fast = fast->next->next ;
+            if(slow==fast)
+            return true;
+            
+        }
+        return false;
+    }
