d6f140ea66dbb9074ba92c2a264486703009b127
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-04 17:28:53 +0530

    optimize way

--- Stack/LargestRectangle.cpp
+++ Stack/LargestRectangle.cpp
@@ -1,70 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> findNSL(vector<int>& arr)
-    {
-        int n = arr.size();
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && arr[i]<=arr[st.top()])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                ans[i]=-1;
-            }
-            else{
-                ans[i]=st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> findNSR(vector<int>& arr)
-    {
-        int n = arr.size();
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && arr[i]<=arr[st.top()])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                ans[i]=n;
-            }
-            else{
-                ans[i]=st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-   void  print(vector<int>&  arr)
-    {
-        int n = arr.size();
-        for(int i=0;i<n;i++)
-        {
-            cout<<arr[i]<<" ";
+// vector<int> findNSL(vector<int>& arr)
+//     {
+//         int n = arr.size();
+//         vector<int>ans(n);
+//         stack<int>st;
+//         for(int i=0;i<n;i++)
+//         {
+//             while(!st.empty() && arr[i]<=arr[st.top()])
+//             {
+//                 st.pop();
+//             }
+//             if(st.empty())
+//             {
+//                 ans[i]=-1;
+//             }
+//             else{
+//                 ans[i]=st.top();
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+//     }
+//     vector<int> findNSR(vector<int>& arr)
+//     {
+//         int n = arr.size();
+//         vector<int>ans(n);
+//         stack<int>st;
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             while(!st.empty() && arr[i]<=arr[st.top()])
+//             {
+//                 st.pop();
+//             }
+//             if(st.empty())
+//             {
+//                 ans[i]=n;
+//             }
+//             else{
+//                 ans[i]=st.top();
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+//     }
+//    void  print(vector<int>&  arr)
+//     {
+//         int n = arr.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             cout<<arr[i]<<" ";
 
-        }
-        cout<<endl;
-    }
-    int largestRectangleArea(vector<int>& heights) 
+//         }
+//         cout<<endl;
+//     }
+//     int largestRectangleArea(vector<int>& heights) 
+//     {
+//         vector<int> NSL = findNSL(heights);
+//         vector<int>NSR = findNSR(heights);
+//         int res = 0;
+//         print(NSL);
+//         print(NSR);
+//         for(int i=0;i<heights.size();i++)
+//         {
+//            res = max(res , (NSR[i]-NSL[i]-1)* heights[i]);
+//         }
+//         return res;
+//     }
+
+//optimize method 
+int getMaxArea(vector<int> &arr) {
+    // Your code here
+    int n = arr.size();
+    stack<int>st;
+    int res = 0;
+    for(int i=0;i<n;i++)
     {
-        vector<int> NSL = findNSL(heights);
-        vector<int>NSR = findNSR(heights);
-        int res = 0;
-        print(NSL);
-        print(NSR);
-        for(int i=0;i<heights.size();i++)
+        while(!st.empty() && arr[i]<arr[st.top()])
         {
-           res = max(res , (NSR[i]-NSL[i]-1)* heights[i]);
-        }
-        return res;
-    }
+            int index = st.top();
+            st.pop();
+            int nsl = i;
+            int psl = st.empty()?-1 :st.top();
+            
+            res = max(res,(nsl-psl-1)*arr[index]);
+         }
+         st.push(i);
+     }
+     
+     while(!st.empty())
+     {
+         int index = st.top();
+         st.pop();
+         int psl = st.empty()?-1 :st.top();
+         res = max(res,(n-psl-1)*arr[index]);
+         
+     }
+     return res;
+}
