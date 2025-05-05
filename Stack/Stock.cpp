f6fa5245a6f3d065374ef1f8b92f46672859dd94
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-05 10:50:03 +0530

    optimize way

--- Stack/Stock.cpp
+++ Stack/Stock.cpp
@@ -1,32 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class StockSpanner
- {
+// class StockSpanner
+//  {
     
-        public:
-       vector<int>arr;
-            StockSpanner() 
-            {
+//         public:
+//        vector<int>arr;
+//             StockSpanner() 
+//             {
                 
-            }
+//             }
             
-            int next(int price) {
-                arr.push_back(price);
-               int  count = 1;
+//             int next(int price) {
+//                 arr.push_back(price);
+//                int  count = 1;
 
-                for(int i = arr.size()-2;i>=0;i--)
-                {
-                      if(arr[i]<=price)
-                      {
-                        count++;
+//                 for(int i = arr.size()-2;i>=0;i--)
+//                 {
+//                       if(arr[i]<=price)
+//                       {
+//                         count++;
 
-                      }
-                      else
-                      break;
-                }
-                return count;
+//                       }
+//                       else
+//                       break;
+//                 }
+//                 return count;
 
-            }
-};
+//             }
+// };
 
+stack<pair<int,int>>st;
+int index =-1;
+StockSpanner() {
+
+   
+}
+
+int next(int price) {
+        if(!st.empty())
+   cout<<st.top().second<<" ";
+   index = index + 1;
+   while(!st.empty() && st.top().first <=price)
+   {
+       st.pop();
+   }
+ 
+   int res = index - (st.empty()?-1:st.top().second);
+   st.push({price,index});
+   return res;
+
+   
+}
