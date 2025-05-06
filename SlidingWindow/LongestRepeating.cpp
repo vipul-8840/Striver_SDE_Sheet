4912317c5d8c64b094f8d0c6d8e959a0ee8ac6af
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-06 16:06:10 +0530

    slidding window

--- SlidingWindow/LongestRepeating.cpp
+++ SlidingWindow/LongestRepeating.cpp
@@ -4,29 +4,58 @@ using namespace std;
 
 
 
-int characterReplacement(string s, int k)
+// int characterReplacement(string s, int k)
+//     {
+//          int n = s.size();
+//          int res=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             int hash[26]={0};
+//             int maxfrq=0;
+//           for(int j=i;j<n;j++)
+//           {
+//              hash[s[j]-'A']++;
+
+//              maxfrq=max(maxfrq,hash[s[j]-'A']);
+//              int changes = (j-i+1)-maxfrq;
+//              if(changes<=k)
+//              {
+//                 res = max(res,j-i+1);
+//              }
+//              else{
+//                 break;
+//              }
+
+//           }
+//         }
+//         return res;  
+//     }
+
+
+int characterReplacement(string s, int k) 
     {
-         int n = s.size();
-         int res=0;
-        for(int i=0;i<n;i++)
+        int res = 0;
+        int maxfeq=0;
+        int n = s.size();
+        int end = 0;
+        int start = 0;
+        int hash[26]={0};
+        int maxfreq = 0;
+        while(end<n)
         {
-            int hash[26]={0};
-            int maxfrq=0;
-          for(int j=i;j<n;j++)
-          {
-             hash[s[j]-'A']++;
-
-             maxfrq=max(maxfrq,hash[s[j]-'A']);
-             int changes = (j-i+1)-maxfrq;
-             if(changes<=k)
+             hash[s[end]-'A']++;
+             maxfreq=max(hash[s[end]-'A'],maxfreq);
+             int len = end-start+1;
+             if(len-maxfreq>k)
              {
-                res = max(res,j-i+1);
+                hash[s[start]-'A']--;
+                start++;
+               
              }
-             else{
-                break;
-             }
+            
+             res = max(res,end-start+1);          
+             end++;
 
-          }
         }
-        return res;  
+        return res;
     }
