78e0a9b4251955a08fd93645ae13013f192458dd
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-07 22:59:00 +0530

    optimzation

--- SlidingWindow/SubarrayK.cpp
+++ SlidingWindow/SubarrayK.cpp
@@ -1,23 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int subarraysWithKDistinct(vector<int>& nums, int k)
+// int subarraysWithKDistinct(vector<int>& nums, int k)
      
-{
-   int n = nums.size();
-   int count =0;
-   for(int i=0;i<n;i++)
-   {
-       unordered_map<int,int>m;
-       for(int j=i;j<n;j++)
-       {
-          m[nums[j]]++;
-          if(m.size()==k)
-          count++;
+// {
+//    int n = nums.size();
+//    int count =0;
+//    for(int i=0;i<n;i++)
+//    {
+//        unordered_map<int,int>m;
+//        for(int j=i;j<n;j++)
+//        {
+//           m[nums[j]]++;
+//           if(m.size()==k)
+//           count++;
 
-          if(m.size()>k)
-          break;
-       }
+//           if(m.size()>k)
+//           break;
+//        }
+//    }
+//    return count;
+// }
+
+//optimize way
+
+int finding(vector<int>& nums,int k)
+{
+   if(k<0)
+   return 0;
+   int start = 0,res=0;
+   int end = 0;
+   unordered_map<int,int>m;
+   int n = nums.size();
+   while(end<n)
+   {
+      m[nums[end]]++;
+      while(start<=end && m.size()>k)
+      {
+           m[nums[start]]--;
+           if(m[nums[start]]==0)
+           {
+               m.erase(nums[start]);
+           }
+           start++;
+      }
+      res=res+(end-start+1);
+      end++;
    }
-   return count;
+   return res;
+}
+int subarraysWithKDistinct(vector<int>& nums, int k)
+{
+   
+   int res1=finding(nums,k);
+   int res2 = finding(nums,k-1);
+   return (res1-res2);
+
 }
