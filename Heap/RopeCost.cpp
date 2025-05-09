b6419f8ab10c9b59ede5e8261268e3cba5b6942f
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-09 23:06:56 +0530

    ropes cost

---
+++ Heap/RopeCost.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minCost(vector<int>& arr)
+ {
+        // code here
+        if(arr.size()<=1)
+        return 0;
+         priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.end());
+        int res = 0;
+        while(pq.size()>1)
+        {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+             int c = a+b;
+             pq.push(c);
+             res+=c;
+            
+        }
+        return res;
+    }
