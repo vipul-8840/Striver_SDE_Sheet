34c5b632696d94325845585598ed42ef35345003
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-09 22:08:29 +0530

    priority min

--- Heap/Priority_Queue.cpp
+++ Heap/Priority_Queue.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main ()
 {
-    priority_queue<int>pq;
-    pq.push(10);
-    pq.push(20);
+    // priority_queue<int>pq;
+    // pq.push(10);
+    // pq.push(20);
+    // cout<<pq.top()<<endl;
+    // pq.pop();
+    // cout<<pq.size()<<endl;
+    priority_queue<int,vector<int>,greater<int>>pq;
+    pq.push(90);
+    pq.push(2);
+    pq.push(6);
+    pq.push(80);
     cout<<pq.top()<<endl;
     pq.pop();
-    cout<<pq.size()<<endl;
+    cout<<pq.top()<<endl;
+    cout<<pq.size();
     return 0;
 }
