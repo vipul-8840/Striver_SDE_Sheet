845ba2e7bf65dd87cd86f3646673be42026159e1
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-10 11:04:51 +0530

    kth smallest using min heap

--- Heap/KthSmallest.cpp
+++ Heap/KthSmallest.cpp
@@ -1,10 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int kthSmallest(vector<int> &arr, int k)
-    {
-        // code here
-        sort(arr.begin(),arr.end());
-        return arr[k-1];
+// int kthSmallest(vector<int> &arr, int k)
+//     {
+//         // code here
+//         sort(arr.begin(),arr.end());
+//         return arr[k-1];
         
-    }
+//     }
+
+     int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>>pq(arr.begin(),arr.end());
+        
+        while(k>1 && !pq.empty())
+        {
+           pq.pop();
+           k--;
+            
+        }
+        
+        return pq.top();
+        
+        
