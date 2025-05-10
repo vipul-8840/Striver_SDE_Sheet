213b6d7dbf4f031511591056b65c8c0bd78e54fc
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-10 11:44:17 +0530

    kth largest

---
+++ Heap/KthLargest.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ int findKthLargest(vector<int>& nums, int k) 
+    {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int n = nums.size();
+        for(int i=0;i<k;i++)
+        {
+            pq.push(nums[i]);
+
+        }
+        
+        for(int i=k;i<n;i++)
+        {
+           if(nums[i]>pq.top())
+           {
+            pq.pop();
+            pq.push(nums[i]);
+           }
+        }
+       return pq.top();
+
+    }
