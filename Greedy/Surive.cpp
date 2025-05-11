ca3ab28055afa824fbc6cd456fac94b810b04442
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-11 23:57:09 +0530

    surive

--- Greedy/Surive.cpp
+++ Greedy/Surive.cpp
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--){
-       
+int minimumDays(int S, int N, int M) {
+        // code here
+        if(M > N) return -1;
+        if((S >= 7) && (N-M)*6 < M) return -1;
+        
+        int toatalFood = S*M;
+        
+        if(toatalFood%N == 0){
+            return (toatalFood/N);
+        }
+        else{
+            return ((toatalFood/N)+1);
+        }
     }
-    return 0;
-}
