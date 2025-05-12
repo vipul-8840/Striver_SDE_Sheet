93c1f4467311d7dd67b0e572f523cba15ffcad71
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-12 17:39:44 +0530

    sjf

---
+++ Greedy/Jump.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canJump(vector<int>& nums) 
+    {
+        
+        int maxIndex = 0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(i>maxIndex)
+            return false;
+
+            maxIndex = max(maxIndex,i+nums[i]);
+        }
+        if(maxIndex>=nums.size()-1)
+        return true;
+
+        return false;
+    }
