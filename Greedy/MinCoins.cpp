748b20be0e4cbe564af2c171eaa35c673459d575
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-14 02:00:02 +0530

    min coins req

--- Greedy/MinCoins.cpp
+++ Greedy/MinCoins.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--){
-       
+vector<int> minPartition(int N)
+    {
+        
+        int currency[]={2000,500,200,100,50,20,10,5,2,1};
+        vector<int>ans;
+        int i=0;
+        while(N)
+        {
+            int a =  N/currency[i];
+            
+            while(a--)
+            {
+                ans.push_back(currency[i]);
+             
+            }
+            N = N%currency[i];
+            i++;
+        }
+        
+        return ans;
     }
-    return 0;
-}
