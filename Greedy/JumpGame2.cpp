1f98acb13b2ab2f4ab62c43392044f645e628379
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-13 13:29:20 +0530

    jump game

---
+++ Greedy/JumpGame2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+  int minJump(vector<int>&nums,int index,int count)
+   {
+    int n = nums.size();
+      
+      if(index>=n-1)
+      return count;
+      if(nums[index]==0)
+      {
+        return nums.size()+1;
+      }
+       int mini = nums.size()+1;
+
+      for(int i=1;i<=nums[index];i++)
+      {
+         mini = min (mini,minJump(nums,index+i,count+1));
+      }
+
+      return mini;
+
+
+   }
+    int jump(vector<int>& nums) {
+          int n = nums.size()+1;
+    int a =  minJump(nums,0,0);
+    return  n==a?-1:a;
+        
+    }
