f973b905bb527ce3b1ccf5ef624bb34e28c7f165
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-15 07:28:47 +0530

    list rep in graph

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -27,3 +27,18 @@ using namespace std;
 // space complexity are uses in adjaency matrix O(n^2);
 // but when we use adjaency list we save space complexity  and it's S.C. O(2*edges).
 
+
+int main ()
+{
+    int n ,m ;
+    cin>>n>>m;
+    vector<int>adj[n];
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return 0;
+}
