c57b28841dd7684664e7386d5c262e8ff3a8116a
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-15 07:07:25 +0530

    basic terminology in graph

---
+++ Graph/graph.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// path : its contains a node which are reachable and at most one .
+// no of degree in undirected graph = 2 * edges 
+// graph represents in two ways 1. adjaency matrix 2. adjanecy list .
--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 
 // java.utils;
 
-class Main {
+// class Main {
 
 
-    public static void main(String args[])
-    {
-          System.out.println("Hello Vipul");
-          System.err.println("hello");
+//     public static void main(String args[])
+//     {
+//           System.out.println("Hello Vipul");
+//           System.err.println("hello");
           
-    }
+//     }
 
-}
+// }
+
+//graph here we started 
+
+
   
