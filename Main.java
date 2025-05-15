f673f56bea5ed6219c40e08172393de6ea540bc6
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-15 08:05:36 +0530

    list rep in graph in java

--- Main.java
+++ Main.java
@@ -14,6 +14,25 @@
 // }
 
 //graph here we started 
+import java.util.Scanner;
+class Main{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n ,m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+      int adj [][]=new int[n][n];
+        for(int i=0;i<m;i++)
+        {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj[u][v]=1;
+            adj[v][u]=1;
+        }
 
-
+        sc.close();
+         
+    }
+}
   
