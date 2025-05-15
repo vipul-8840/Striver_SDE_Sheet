9a0eb05f36ee51391e20361d39bd2829836adfb7
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-15 07:18:18 +0530

    representation in graph

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -4,3 +4,22 @@ using namespace std;
 // path : its contains a node which are reachable and at most one .
 // no of degree in undirected graph = 2 * edges 
 // graph represents in two ways 1. adjaency matrix 2. adjanecy list .
+
+//Matrix Representation 
+
+int main ()
+{
+    int n ,m;
+    cin>>n>>m;
+    vector<vector<int>>adj(n,vector<int>(n,0));
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+
+    }
+
+    return 0;
+}
