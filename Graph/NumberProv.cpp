7f5d5c99ac52d08492bd8e91eeaa88783d057ac7
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-16 09:31:01 +0530

    optimize way

--- Graph/NumberProv.cpp
+++ Graph/NumberProv.cpp
@@ -1,16 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<int>adj[],vector<int>& vis,int node,vector<int>&parent ,int index)
+// void dfs(vector<int>adj[],vector<int>& vis,int node,vector<int>&parent ,int index)
+//    {
+//       vis[node]=1;
+//       parent.push_back(index);
+//       for(int i=0;i<adj[node].size();i++)
+//       {
+//           int neighbour = adj[node][i];
+//           if(!vis[neighbour])
+//           {
+//               dfs(adj,vis,neighbour,parent,index);
+//           }
+//       }
+
+//    }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n  = isConnected.size();
+//         vector<int>adj[n];
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(i!=j && isConnected[i][j]==1)
+//                 {
+//                     adj[i].push_back(j);
+//                 }
+//             }
+//         }
+
+//         vector<int>vis(n,0);
+//         vector<int>parent;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(!vis[i])
+//             {
+//                 dfs(adj,vis,i,parent,i);
+//             }
+//         }
+//         int count = 1;
+//         for(int i=1;i<n;i++)
+//         {
+//             if(parent[i]!=parent[i-1])
+//             {
+//                   count++;
+//             }
+//         }
+//         return count;
+//     }
+
+void dfs(vector<int>adj[],vector<int>& vis,int node)
    {
       vis[node]=1;
-      parent.push_back(index);
+   
       for(int i=0;i<adj[node].size();i++)
       {
           int neighbour = adj[node][i];
           if(!vis[neighbour])
           {
-              dfs(adj,vis,neighbour,parent,index);
+              dfs(adj,vis,neighbour);
           }
       }
 
@@ -18,6 +66,8 @@ void dfs(vector<int>adj[],vector<int>& vis,int node,vector<int>&parent ,int inde
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n  = isConnected.size();
         vector<int>adj[n];
+        vector<int>vis(n);
+        int count = 0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
@@ -28,23 +78,15 @@ void dfs(vector<int>adj[],vector<int>& vis,int node,vector<int>&parent ,int inde
                 }
             }
         }
-
-        vector<int>vis(n,0);
-        vector<int>parent;
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
-                dfs(adj,vis,i,parent,i);
+                count++;
+                dfs(adj,vis,i);
             }
         }
-        int count = 1;
-        for(int i=1;i<n;i++)
-        {
-            if(parent[i]!=parent[i-1])
-            {
-                  count++;
-            }
-        }
+     
+        
         return count;
     }
