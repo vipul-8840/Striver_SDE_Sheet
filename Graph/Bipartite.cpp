7870e613aee20829750caa6b461c7d0e29dbb007
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-17 21:46:57 +0530

    bipartie

---
+++ Graph/Bipartite.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+   bool check(vector<vector<int>>& adj,vector<int>&color ,int node)
+   {
+       color[node]=0;
+       queue<int>q;
+       q.push(node);
+       while(!q.empty())
+       {
+        int node = q.front();
+        q.pop();
+          for(int i=0;i<adj[node].size();i++)
+          {
+              int neighbour= adj[node][i];
+              if(color[neighbour]==-1)
+              {
+                color[neighbour]=!color[node];
+                q.push(neighbour);
+              }
+              else{
+                   if(color[neighbour]==color[node])
+                   return false;
+              }
+          }
+       }
+       return true;
+   }
+    bool isBipartite(vector<vector<int>>& adj)
+     {
+        
+          int m = adj.size();
+          vector<int>color(m,-1);
+          for(int i=0;i<m;i++)
+          {
+            if(color[i]==-1)
+               if(!check(adj,color,i))
+               {
+                return false;
+               }
+          }
+          return true;
+    }
+};
