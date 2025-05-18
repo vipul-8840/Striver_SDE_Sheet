9af2e1ae88878a2bfc9342a1fabb2ef91c712a42
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-18 18:06:46 +0530

    kahn's algo

--- Graph/topoSort.cpp
+++ Graph/topoSort.cpp
@@ -1,44 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- void dfs (vector<int>adj[],int node,int vis[],stack<int>&st)
-   {
-         vis[node]=1;
-         for(int i=0;i<adj[node].size();i++)
-         {
-             int adjNode = adj[node][i];
-             if(!vis[adjNode])
-             {
-                 dfs(adj,adjNode,vis,st);
-             }
-         }
-         st.push(node);
+//  void dfs (vector<int>adj[],int node,int vis[],stack<int>&st)
+//    {
+//          vis[node]=1;
+//          for(int i=0;i<adj[node].size();i++)
+//          {
+//              int adjNode = adj[node][i];
+//              if(!vis[adjNode])
+//              {
+//                  dfs(adj,adjNode,vis,st);
+//              }
+//          }
+//          st.push(node);
          
-   }
-    vector<int> topoSort(int V, vector<vector<int>>& edges) 
-    {
-        // code here
-        vector<int>adj[V];
-        int vis[V]={0};
-        stack<int>st;
+//    }
+//     vector<int> topoSort(int V, vector<vector<int>>& edges) 
+//     {
+//         // code here
+//         vector<int>adj[V];
+//         int vis[V]={0};
+//         stack<int>st;
         
+//         for(int i=0;i<edges.size();i++)
+//         {
+//             adj[edges[i][0]].push_back(edges[i][1]);
+//         }
+        
+//         for(int i=0;i<V;i++)
+//         {
+//             if(!vis[i])
+//             {
+//                 dfs(adj,i,vis,st);
+//             }
+//         }
+//         vector<int>ans;
+//         while(!st.empty())
+//         {
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+//         return ans;
+//     }
+
+//  bfs traversal algorithm khan's algorithm 
+vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int>inDeg(V);
+        
+        vector<int>adj[V];
         for(int i=0;i<edges.size();i++)
         {
             adj[edges[i][0]].push_back(edges[i][1]);
         }
         
+        for(int i=0;i<edges.size();i++)
+        {
+            inDeg[edges[i][1]]++;
+        }
+        
+        queue<int>q;
         for(int i=0;i<V;i++)
         {
-            if(!vis[i])
+            if(inDeg[i]==0)
             {
-                dfs(adj,i,vis,st);
+                q.push(i);
             }
         }
+        
         vector<int>ans;
-        while(!st.empty())
+        while(!q.empty())
         {
-            ans.push_back(st.top());
-            st.pop();
+            int node = q.front();
+            ans.push_back(node);
+            q.pop();
+            for(int i=0;i<adj[node].size();i++)
+            {
+                int adjNode = adj[node][i];
+                inDeg[adjNode]--;
+                if(inDeg[adjNode]==0)
+                q.push(adjNode);
+            }
         }
         return ans;
     }
