7e41a5bc55b6de8adf9a2f0d17aefb0bf37cbcee
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-18 19:12:04 +0530

    course sche

--- Graph/Course.cpp
+++ Graph/Course.cpp
@@ -1,16 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canFinish(int V, vector<vector<int>>& pre)
-     {
-          vector<vector<int>> adj(V);
+// bool canFinish(int V, vector<vector<int>>& pre)
+//      {
+//           vector<vector<int>> adj(V);
+//         vector<int> Indeg(V, 0);
+//         queue<int> q;
+//         vector<int> ans;
+
+//         for(int i = 0; i < pre.size(); i++) {
+//             adj[pre[i][0]].push_back(pre[i][1]);
+//             Indeg[pre[i][1]]++;
+//         }
+
+//         for(int i = 0; i < V; i++) {
+//             if(Indeg[i] == 0)
+//                 q.push(i);
+//         }
+
+//         while(!q.empty()) {
+//             int node = q.front();
+//             q.pop();
+//             ans.push_back(node);
+
+//             for(int adjNode : adj[node]) {
+//                 Indeg[adjNode]--;
+//                 if(Indeg[adjNode] == 0)
+//                     q.push(adjNode);
+//             }
+//         }
+
+//         return ans.size() == V;
+//     }
+
+
+ vector<int> findOrder(int V, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(V);
         vector<int> Indeg(V, 0);
         queue<int> q;
         vector<int> ans;
 
         for(int i = 0; i < pre.size(); i++) {
-            adj[pre[i][0]].push_back(pre[i][1]);
-            Indeg[pre[i][1]]++;
+            adj[pre[i][1]].push_back(pre[i][0]);
+            Indeg[pre[i][0]]++;
         }
 
         for(int i = 0; i < V; i++) {
@@ -30,5 +62,8 @@ bool canFinish(int V, vector<vector<int>>& pre)
             }
         }
 
-        return ans.size() == V;
+        
+        vector<int>temp;
+        return ans.size() == V ? ans :temp;
+        
     }
