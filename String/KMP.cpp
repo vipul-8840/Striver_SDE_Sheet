e0d6ff97cfb8f7ce6a3decd06c2f97153a06d9bd
vipul-8840 <vipulkumarlen@gmail.com>
2025-05-26 11:09:31 +0530

    longest prefix sum

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -2,3 +2,32 @@
 using namespace std;
 
 //longest prefix suffix
+ int longestPrefixSuffix(string &s) {
+        // Your code goes here
+        int n = s.size();
+        vector<int>lps(n,0);
+        int pre = 0;
+       int suf = 1;
+        lps[0]=0;
+        
+       while(suf<n)
+        {
+           if(s[pre]==s[suf])
+           {
+               lps[suf]=pre+1;
+               suf++;
+               pre++;
+           }
+           else{
+               if(pre==0)
+               {
+                   lps[suf]=0;
+                   suf++;
+               }
+               else{
+                   pre = lps[pre-1];
+               }
+           }
+        }
+        return lps[n-1];
+    }
